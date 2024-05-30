12e0817d4420300b3674e33520d18f02df53c6ed
Noah Calvo <Noah.Calvo@target.com>
2024-05-30 00:17:25 -0500

    wrap login in useSuspense

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,14 +1,25 @@
-'use client'
-
+'use client';
 import LoginForm from '@/app/ui/auth/login-form';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function LoginPage() {
-  const searchParams = useSearchParams()
- 
-  const success = searchParams.get('success')
-  console.log(success)
-  console.log(searchParams)
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginPageContent />
+      </Suspense>
+    </div>
+  );
+}
+
+function LoginPageContent() {
+  const searchParams = useSearchParams();
+  const success = searchParams.get('success');
+
+  console.log(success);
+  console.log(searchParams);
+
   return (
     <div>
       {success && <p>Account successfully created for {success}!</p>}
