95ad2922b4941ff30c75894389985de4872dec4a
Noah Calvo <Noah.Calvo@target.com>
2024-06-21 00:15:46 -0500

    Create page.tsx

---
+++ app/league/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+    return (
+      <main className="">
+          {/* <div className='text-black'>Try selecting one of the above options</div> */}
+      </main>
+    );
+  }
