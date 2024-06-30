e4c2a866267af02dbcba9744e8be429b2d85cb41
Noah Calvo <noah.calvo@gmail.com>
2024-06-30 01:33:04 -0500

    fix cron job

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -3,14 +3,11 @@ import { runMondayTask } from "@/app/lib/performance";
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export async function GET(req: Request) {
-  const secretKey = req.headers.get('x-cron-secret-key');
-  console.log(secretKey);
-  console.log(process.env.CRON_SECRET_KEY);
 
-  if (secretKey !== process.env.CRON_SECRET_KEY) {
+  const authHeader = req.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response("hellow child");;
   }
-
-  await runMondayTask();
+      await runMondayTask();
   return new Response(`Rosters were processed`);
 }
